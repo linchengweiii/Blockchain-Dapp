ff5b5017e039c5ee022830898bc59585d243d409
andyh0913 <andyh0913@gmail.com>
2019-06-10 22:58:32 +0800

    add web3 test files

---
+++ test_server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+var bodyParser = require('body-parser')
+var port = process.env.PORT || 4000
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'web3test.html'))
+})
+
+
+
+server = app.listen(port , () => console.log('Listening on port ' + port))
